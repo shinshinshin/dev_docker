4fcb0a0ec64819371beca25b2a8446b9e69fd0eb
shinshinshin <saekishinya@yahoo.co.jp>
2022-12-17 14:55:43 +0900

    init

---
+++ mysql/initdb.d/initial.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE IF NOT EXISTS sample;
+USE sample;
+
+CREATE TABLE IF NOT EXISTS sample_table
+(
+  `id`         int(11) NOT NULL AUTO_INCREMENT,
+  `sample`     text,
+  PRIMARY KEY (`id`)
+);
+
+INSERT INTO sample_table (sample) VALUES ("sample1");
+INSERT INTO sample_table (sample) VALUES ("sample2");
